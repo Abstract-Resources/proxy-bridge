0d5c2b9253cd647ab12e7fe0a2d5773b354196b8
Xavier <xavier04.martinez@gmail.com>
2022-11-02 20:20:34 -0600

    feat: VanillaBridgePacketHandler documentation added

--- shared/src/main/java/dev/aabstractt/bridge/datasource/protocol/handler/VanillaBridgePacketHandler.java
+++ shared/src/main/java/dev/aabstractt/bridge/datasource/protocol/handler/VanillaBridgePacketHandler.java
@@ -2,6 +2,7 @@ package dev.aabstractt.bridge.datasource.protocol.handler;
 
 import dev.aabstractt.bridge.datasource.protocol.ProxyStatusUpdatePacket;
 
+@SuppressWarnings("unused") // SuppressWarning of unused methods because all methods here is handled on RedisDataSource#handlePacket
 public final class VanillaBridgePacketHandler {
 
     public void handleProxyStatusUpdatePacket(ProxyStatusUpdatePacket pk) {
